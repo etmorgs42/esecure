0c6d36bac397d3aae00e018fd8f79688d52080d4
Ed Morgans <eddie.morgans@gmail.com>
2013-03-23 20:08:14 -0400

    I added a point class with some private variables and a constructor

--- ESecure/src/embedded2/ESecure/Attempt.java
+++ ESecure/src/embedded2/ESecure/Attempt.java
@@ -4,5 +4,5 @@ import java.util.ArrayList;
 
 public class Attempt {
 	ArrayList<Point> points;
-	
+	ArrayList<Integer> gaps;
 }
---
+++ ESecure/src/embedded2/ESecure/Point.java
@@ -0,0 +1,17 @@
+package embedded2.ESecure;
+
+import java.util.Date;
+
+public class Point {
+	float x,y;
+	float pressure;
+	float majorAxis,minorAxis; //major and minor axes of touch ellipse
+	float orientation; //orientation of touch ellipse
+	Date start,end;
+	char keyPress;
+	
+	Point(int x, int y, Date start){
+		
+	}
+
+}
