8c39270c24f4c6a20b1913f2a2490a6e609692fd
Ed Morgans <eddie.morgans@gmail.com>
2013-03-25 17:23:12 -0400

    Finished (mostly) the point class

--- ESecure/src/embedded2/ESecure/Point.java
+++ ESecure/src/embedded2/ESecure/Point.java
@@ -3,7 +3,8 @@ package embedded2.ESecure;
 import java.util.Date;
 
 public class Point {
-	float x,y;
+	int x,y;
+	long duration;
 	float pressure;
 	float majorAxis,minorAxis; //major and minor axes of touch ellipse
 	float orientation; //orientation of touch ellipse
@@ -11,7 +12,45 @@ public class Point {
 	char keyPress;
 	
 	Point(int x, int y, Date start){
-		
+		this.x = x;
+		this.y = y;
+		this.start = start;
 	}
+	
+	void setShape(float majorAxis, float minorAxis){
+		this.majorAxis = majorAxis;
+		this.minorAxis = minorAxis;
+	}
+	
+	void setOrientation(float orientation){
+		this.orientation = orientation;
+	}
+	
+	void setKey(char keyPress){
+		this.keyPress = keyPress;
+	}
+	
+	void setPressure(float pressure){
+		this.pressure = pressure;
+	}
+	
+	void setEnd(Date end){
+		this.end = end;
+		duration = end.getTime() - start.getTime();
+	}
+	
+	int getX(){ return x; }
+	int getY(){ return y; }
+	float getMajorAxis(){ return majorAxis; }
+	float getMinorAxis(){ return minorAxis; }
+	float getOrientation(){ return orientation; }
+	float getPressure(){ return pressure; }
+	char getKey(){ return keyPress; }
+	Date getStart(){ return start; }
+	Date getEnd(){ return end; }
+	long getDuration(){ return duration; }
+	
+	
+	
 
 }
